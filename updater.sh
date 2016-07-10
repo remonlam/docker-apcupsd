28399da0c2a0eeef136c702730826d1df41b1082
Remon Lam <remon.lam@lamnet.nl>
2016-07-10 11:48:44 +0200

    change download order

--- updater.sh
+++ updater.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+docker pull remonlam/apc-web
 docker stop $(docker ps -a -q)
 docker rm apc-web
-docker pull remonlam/apc-web
+#docker pull remonlam/apc-web
 docker run -d -p 80:80 --name apc-web --device=/dev/usb/hiddev0 remonlam/apc-web:latest /entrypoint.sh
