077b46769cc3fb287254fb18cf6b0fa86230be76
Remon Lam <remon.lam@lamnet.nl>
2016-07-10 13:22:29 +0200

    testing

--- updater.sh
+++ updater.sh
@@ -7,3 +7,4 @@ docker stop $(docker ps -a -q)
 docker rm apc-web
 #docker pull remonlam/apc-web
 docker run -d -p 80:80 --name apc-web --device=/dev/usb/hiddev0 remonlam/apc-web:latest /entrypoint.sh
+docker run -d -p 80:80 --name apc-web --device=/dev/usb/hiddev0 remonlam/apc-web:0.3
