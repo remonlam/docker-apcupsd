01f0549d76ef1dfe27f60e07dc944a66609c49fc
Remon Lam <remon@containerstack.io>
2017-07-08 12:26:26 +0200

    change start order

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo "ACCESS THE UPC WEB PAGE: http://YOUR-PI-UPS/cgi-bin/apcupsd/multimon.cgi"
+
+# Start Postfix mail relay
+postfix start
+
 # Start APC Service
 service apcupsd start
 
 # Start apache2 service
 service apache2 start
-
-# Start Postfix mail relay
-postfix start
 
 # Leave this file  open
 tail -F /var/log/apcupsd.events
