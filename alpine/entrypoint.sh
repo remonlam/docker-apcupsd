06068821badeedf3bf3bb34b95371ec1bd2322a7
Remon Lam <remon@containerstack.io>
2018-07-17 10:53:30 +0200

    add sources for alpine port

---
+++ alpine/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "ACCESS THE UPC WEB PAGE: http://YOUR-PI-UPS/cgi-bin/apcupsd/multimon.cgi"
+
+# Set hostname in Postfix config file
+sed -i s/updateNodeName/$HOSTNAME/g /etc/postfix/main.cf
+
+# Set mail relay
+sed -i s/updateMailRelay/$MAILRELAY/g /etc/postfix/main.cf
+
+# Start Postfix mail relay
+postfix start
+
+# Start APC Service
+service apcupsd start
+
+# Start apache2 service
+service apache2 start
+
+# Leave this file  open
+tail -F /var/log/apcupsd.events
